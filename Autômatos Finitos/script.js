6445a2b5e9289b49d346773d5b889a1df47da655
D-Ono <hiromi.ono@unesp.br>
2021-01-02 21:37:39 -0300

    Tentativa falha

---
+++ Autômatos Finitos/script.js
@@ -0,0 +1,48 @@
+const container = document.getElementById("automato");
+const botaoCriarEstado = document.getElementById("criar-estado")
+let aux = 0
+let count = 0
+
+var nodes = new vis.DataSet([ ]);
+
+  // create an array with edges
+var edges = new vis.DataSet([
+   // { from: 1, to: 3 },
+]);
+
+  // create a network
+  var data = {
+    nodes: nodes,
+    edges: edges,
+  };
+
+  var options = {
+    nodes: {
+        physics: false,
+        shape: "circle",
+        color: 'light',
+        font: {
+            size: 30,
+            align: "center"
+        },
+        margin: 20
+    },
+  };
+
+
+let network = new vis.Network(container, data, options);
+
+botaoCriarEstado.addEventListener('click', () => {aux = 1})
+
+function criarEstados() {
+    if(aux == 1){
+        nodes.add({
+            id: count,
+            label: `q${count}`,
+            
+        })
+        count++
+    }
+}
+
+network.addEventListener('click', criarEstados)
